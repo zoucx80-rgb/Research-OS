e47cc2469cda41da7ed437bd88497f0b86a7e53e
zoucx80-rgb <zoucx80@gmail.com>
2026-08-30 01:00:50 +0800

    release: expose v1.4 core api version

--- src/research_os/__init__.py
+++ src/research_os/__init__.py
@@ -1,3 +1,4 @@
-from .version import RESEARCH_OS_VERSION
+from .version import CORE_API_VERSION, RESEARCH_OS_VERSION
 
 __version__ = RESEARCH_OS_VERSION
+__all__ = ["CORE_API_VERSION", "RESEARCH_OS_VERSION", "__version__"]
