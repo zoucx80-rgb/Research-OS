b4e0ae9678bbc2eff6db18fd437da87bc4d5e27c
zoucx80-rgb <zoucx80@gmail.com>
2026-08-30 08:19:54 +0800

    feat: export human-readable reporting adapter

---
+++ src/research_os/reporting/__init__.py
@@ -0,0 +1,15 @@
+from .semantics import (
+    DecisionSummaryPresenter,
+    HumanReadableDecisionSummary,
+    SemanticValue,
+)
+from .summary import DecisionSummary, DecisionSummaryBuilder, ResearchReportModel
+
+__all__ = [
+    "DecisionSummary",
+    "DecisionSummaryBuilder",
+    "DecisionSummaryPresenter",
+    "HumanReadableDecisionSummary",
+    "ResearchReportModel",
+    "SemanticValue",
+]
